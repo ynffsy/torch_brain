d6674f639edfacc616617746c99ee60327940e8b
Mehdi Azabou <mehdiazabou@gmail.com>
2023-04-30 14:38:46 -0400

    add dropout augmentation, nlb maze

--- kirby/transforms/__init__.py
+++ kirby/transforms/__init__.py
@@ -1,1 +1,12 @@
 from .random_crop import RandomCrop
+from .unit_dropout import UnitDropout
+
+
+class Compose:
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, data):
+        for transform in self.transforms:
+            data = transform(data)
+        return data
--- kirby/transforms/unit_dropout.py
+++ kirby/transforms/unit_dropout.py
@@ -1,36 +1,82 @@
 
+import numpy as np
 import torch
-from einops import repeat
+
+
+from kirby.utils import logging
+
+log = logging(header='UNIT DROPOUT', header_color='cyan')
+
+
+class UnitCustomDistribution:
+    def __init__(self, min_units=20, mode_units=100, max_units=300, peak=4, M=10, max_attempts=100, seed=None):
+        super().__init__()
+        self.min_units = min_units
+        self.mode_units = mode_units
+        self.max_units = max_units
+        self.peak = peak
+        self.M = M
+        self.max_attempts = max_attempts
+
+        self.rng = np.random.default_rng(seed=seed)
+
+    def unnormalized_density_function(self, x):
+        if x < self.min_units:
+            return 0
+        if x <= self.mode_units:
+            return 1 + (self.peak-1) * (x - self.min_units) / (self.mode_units - self.min_units)
+        if x <= self.max_units:
+            return self.peak - (self.peak - 1) * (x - self.mode_units) / (self.max_units - self.mode_units)
+        return 1
+    
+    def proposal_distribution(self, x):
+        return self.rng.uniform()
+
+    def sample(self, num_units):
+        if num_units < self.min_units:
+            log.warning(f"Requested {num_units} units, but minimum is {self.min_units}")
+            return num_units
+        # uses rejection sampling
+        num_attempts = 0
+        while True:
+            x = self.min_units + self.rng.uniform() * (num_units - self.min_units)  # Sample from the proposal distribution
+            u = self.rng.uniform()
+            if u <= self.unnormalized_density_function(x) / (self.M * self.proposal_distribution(x)):
+                return x
+            num_attempts += 1
+            if num_attempts > self.max_attempts:
+                # warning
+                log.warning(f"Could not sample from distribution after {num_attempts} attempts, using all units")
+                return num_units
+                # raise RuntimeError(f"Could not sample from distribution after {num_attempts} attempts")
+
 
 class UnitDropout:
-    def __init__(self, p):
-        super().__init__()
-        assert 0 <= p <= 1, f'p must be between 0 and 1, got {p}'
-        self.p = p
+    def __init__(self, *args, **kwargs):
+        self.distribution = UnitCustomDistribution(*args, **kwargs)
 
-    def __call__(self, data, num_units=None):
+    def __call__(self, data):
+        # get units
+        units = data.units.id
+        num_units = len(units)
+
+        num_units_to_sample = int(self.distribution.sample(num_units))
+
+        # shuffle units and take the first num_units_to_sample
+        data.units.id = units[torch.randperm(num_units)][:num_units_to_sample]
+
+        # drop the spikes for the dropped units
         spikes = data.spikes
-        target_rates = data.trials.target_rates
+        timestamps = []
+        unit_ids = []
 
-        if num_units is None:
-            maybe_num_units = data.spikes.max() + 1
-            num_units = maybe_num_units
+        for unit_id in data.units.id:
+            spike_mask = spikes.unit_id == unit_id
 
-        # sample units to drop
-        unit_mask = torch.rand(num_units) > self.p
+            timestamps.append(spikes.timestamps[spike_mask])
+            unit_ids.append(spikes.unit_id[spike_mask])
 
-        # drop units from spikes
-        spike_mask = unit_mask[spikes.unit_id]
-        out = spikes.copy()
-        out.timestamps = spikes.timestamps[spike_mask]
-        out.unit_id = spikes.unit_id[spike_mask]
-        data.spikes = out
+        data.spikes.timestamps = torch.cat(timestamps)
+        data.spikes.unit_id = torch.cat(unit_ids)
 
-        # make target rate for dropped units
-        t = target_rates.size(0)
-        target_rates = target_rates[:, ~unit_mask].reshape(-1)
-        target_units = torch.arange(num_units)[~unit_mask]
-        target_units = repeat(target_units, 'u -> t u', t=t)
-        data.target_rates = target_rates
-        data.target_units = target_units
         return data
