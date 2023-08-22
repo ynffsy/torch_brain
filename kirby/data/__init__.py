779f4446f2ecbad4ef19f0c6e4fa5eee03d18473
Patrick Mineault <patrick.mineault@gmail.com>
2023-08-22 00:10:52 -0400

    Expand taxonomy

--- kirby/data/__init__.py
+++ kirby/data/__init__.py
@@ -1,4 +1,7 @@
-from .data import Data, IrregularTimeSeries, Interval
+from .data import Data, IrregularTimeSeries, Interval, RegularTimeSeries, Probe, Channel
 from .nwb_to_data import nwb_to_data
 from .dataset import Dataset, Collate, prepare_sample
-from .balanced_distributed_sampler import BalancedDistributedSampler, create_node_data_buckets
+from .balanced_distributed_sampler import (
+    BalancedDistributedSampler,
+    create_node_data_buckets,
+)
