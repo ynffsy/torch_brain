676451f6f445cb53e4c99fe15d4b728d3e8c391f
Patrick Mineault <patrick.mineault@gmail.com>
2023-10-05 14:35:17 -0400

    Unit dropout

--- kirby/transforms/unit_dropout.py
+++ kirby/transforms/unit_dropout.py
@@ -1,20 +1,30 @@
-
 import numpy as np
 import torch
 
-
 from kirby.utils import logging
 
-log = logging(header='UNIT DROPOUT', header_color='cyan')
+log = logging(header="UNIT DROPOUT", header_color="cyan")
+
+"""
+Triangular distribution with a peak at mode_units, going from min_units to max_units.
+"""
 
 
 class UnitCustomDistribution:
-    def __init__(self, min_units=20, mode_units=100, max_units=300, clip_units=300, peak=4, M=10, max_attempts=100, seed=None):
+    def __init__(
+        self,
+        min_units=20,
+        mode_units=100,
+        max_units=300,
+        peak=4,
+        M=10,
+        max_attempts=100,
+        seed=None,
+    ):
         super().__init__()
         self.min_units = min_units
         self.mode_units = mode_units
         self.max_units = max_units
-        self.clip_units = clip_units
         self.peak = peak
         self.M = M
         self.max_attempts = max_attempts
@@ -25,11 +35,15 @@ class UnitCustomDistribution:
         if x < self.min_units:
             return 0
         if x <= self.mode_units:
-            return 1 + (self.peak-1) * (x - self.min_units) / (self.mode_units - self.min_units)
+            return 1 + (self.peak - 1) * (x - self.min_units) / (
+                self.mode_units - self.min_units
+            )
         if x <= self.max_units:
-            return self.peak - (self.peak - 1) * (x - self.mode_units) / (self.max_units - self.mode_units)
+            return self.peak - (self.peak - 1) * (x - self.mode_units) / (
+                self.max_units - self.mode_units
+            )
         return 1
-    
+
     def proposal_distribution(self, x):
         return self.rng.uniform()
 
@@ -37,19 +51,25 @@ class UnitCustomDistribution:
         if num_units < self.min_units:
             # log.warning(f"Requested {num_units} units, but minimum is {self.min_units}")
             return num_units
+
         # uses rejection sampling
         num_attempts = 0
         while True:
-            x = self.min_units + self.rng.uniform() * (self.clip_units - self.min_units)  # Sample from the proposal distribution
+            x = self.min_units + self.rng.uniform() * (
+                self.max_units - self.min_units
+            )  # Sample from the proposal distribution
             u = self.rng.uniform()
-            if u <= self.unnormalized_density_function(x) / (self.M * self.proposal_distribution(x)):
+            if u <= self.unnormalized_density_function(x) / (
+                self.M * self.proposal_distribution(x)
+            ):
                 return x
             num_attempts += 1
             if num_attempts > self.max_attempts:
                 # warning
-                log.warning(f"Could not sample from distribution after {num_attempts} attempts, using all units")
+                log.warning(
+                    f"Could not sample from distribution after {num_attempts} attempts, using all units"
+                )
                 return num_units
-                # raise RuntimeError(f"Could not sample from distribution after {num_attempts} attempts")
 
 
 class UnitDropout:
@@ -58,26 +78,36 @@ class UnitDropout:
 
     def __call__(self, data):
         # get units
-        units = data.units.id
-        num_units = len(units)
+        unit_names = data.units.unit_name
+        num_units = len(unit_names)
 
         num_units_to_sample = int(self.distribution.sample(num_units))
 
         # shuffle units and take the first num_units_to_sample
-        data.units.id = units[torch.randperm(num_units)][:num_units_to_sample]
+        keep_indices = sorted(torch.randperm(num_units)[:num_units_to_sample])
 
-        # drop the spikes for the dropped units
-        spikes = data.spikes
-        timestamps = []
-        unit_ids = []
+        # update the units
+        units_keep = data.units.__class__.__new__(data.units.__class__)
+        for key, value in data.units.__dict__.items():
+            if value is not None and isinstance(value, torch.Tensor):
+                units_keep.__dict__[key] = value[keep_indices]
+            if value is not None and isinstance(value, list):
+                units_keep.__dict__[key] = [value[x.item()] for x in keep_indices]
+            else:
+                units_keep.__dict__[key] = None
 
-        for unit_id in data.units.id:
-            spike_mask = spikes.unit_id == unit_id
+        data.units = units_keep
 
-            timestamps.append(spikes.timestamps[spike_mask])
-            unit_ids.append(spikes.unit_id[spike_mask])
+        # Keep only the relevant spikes
+        total_spike_mask = torch.zeros_like(data.spikes.timestamps, dtype=torch.bool)
+        n = np.array(data.spikes.names)
+        for unit_name in data.units.unit_name:
+            total_spike_mask |= n == unit_name
 
-        data.spikes.timestamps = torch.cat(timestamps)
-        data.spikes.unit_id = torch.cat(unit_ids)
+        data.spikes.timestamps = data.spikes.timestamps[total_spike_mask == 1]
+        data.spikes.types = data.spikes.types[total_spike_mask == 1]
+        # Tricky bit: torch doesn't have real booleans, only uint8, so when we convert 
+        # back to numpy we have to cast correctly to obtains the desired effect.
+        data.spikes.names = (n[total_spike_mask.cpu().detach().numpy() == 1]).tolist()
 
         return data
---
+++ tests/test_unit_dropout.py
@@ -0,0 +1,37 @@
+import numpy as np
+import torch
+from kirby.data.data import Data, IrregularTimeSeries
+from kirby.transforms.unit_dropout import UnitCustomDistribution, UnitDropout
+
+
+def test_distro():
+    for i in range(100):
+        num_units = UnitCustomDistribution(
+            min_units=100, mode_units=150, max_units=200
+        ).sample(196)
+        assert num_units >= 100 and num_units <= 200
+
+
+def test_spikes():
+    timestamps = torch.zeros(100)
+    names = ["a"] * 10 + ["b"] * 20 + ["c"] * 70
+    names = np.array(names)
+    types = torch.zeros(100)
+
+    for i in range(100):
+        data = Data(
+            spikes=IrregularTimeSeries(
+                timestamps=timestamps,
+                names=names,
+                types=types,
+            ),
+            units=Data(
+                unit_name=["a", "b", "c"],
+            ),
+        )
+        do = UnitDropout(min_units=1, mode_units=2, max_units=3)
+        data_t = do(data)
+        assert data_t.spikes.timestamps.shape[0] in (10, 20, 30, 70, 80, 90, 100)
+        print(np.unique(data_t.spikes.names), data_t.units.unit_name)
+        assert len(np.unique(data_t.spikes.names)) == len(data_t.units.unit_name)
+        assert len(data_t.spikes.timestamps) == len(data_t.spikes.names)
