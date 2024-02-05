4e366d2a4dd1e0dadfaed020f53c50df782665ad
Mehdi Azabou <mehdiazabou@gmail.com>
2024-02-05 02:31:20 -0500

    fix import error

--- kirby/nn/__init__.py
+++ kirby/nn/__init__.py
@@ -11,6 +11,5 @@ from .perceiver_rotary import PerceiverRotary
 from .loss import compute_loss_or_metric
 from .multitask_readout import (
     MultitaskReadout,
-    multitask_readout_tokenzier,
     prepare_for_multitask_readout,
 )
