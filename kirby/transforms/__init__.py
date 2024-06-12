d0526bb8b844a40a5f7afe1952f4e7ab08faa9c5
Mehdi Azabou <36160333+mazabou@users.noreply.github.com>
2024-06-11 22:47:45 -0400

    add various transforms containers (#272)
    * add various transforms containers
    
    * fix call
    
    * catch condition error

--- kirby/transforms/__init__.py
+++ kirby/transforms/__init__.py
@@ -1,19 +1,5 @@
+from .container import Compose, RandomChoice, ConditionalChoice
 from .unit_dropout import UnitDropout, TriangleDistribution
 from .random_time_scaling import RandomTimeScaling
 from .random_crop import RandomCrop
 from .output_sampler import RandomOutputSampler
-
-
-class Compose:
-    r"""Compose several transforms together. All transforms will be called sequentially,
-    in order, and must accept and return a single :obj:`kirby.data.Data` object, except
-    the last transform, which can return any object.
-    """
-
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def __call__(self, data):
-        for transform in self.transforms:
-            data = transform(data)
-        return data
