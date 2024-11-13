8eca64a80fba2cf1897baea2bd8013e325326ee7
Vinam Arora <vinam@posteo.de>
2024-11-13 17:55:08 -0500

    Fix circular import in torch_brain.models (fix #26)

--- torch_brain/utils/__init__.py
+++ torch_brain/utils/__init__.py
@@ -2,5 +2,3 @@ from .tokenizers import create_linspace_latent_tokens, create_start_end_unit_tok
 from .sinusoidal_encoding import get_sinusoidal_encoding
 
 from .seed_everything import seed_everything
-
-from .datamodules import DataModule
