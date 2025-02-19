c43698a1cb6d721aee3e9b931a2ec24b152cb116
Vinam Arora <vinam@posteo.de>
2025-02-18 22:37:27 -0500

    Remove unused import

--- examples/poyo/train.py
+++ examples/poyo/train.py
@@ -16,7 +16,7 @@ from lightning.pytorch.callbacks import (
 from omegaconf import DictConfig, OmegaConf
 import wandb
 
-from torch_brain.registry import MODALITIY_REGISTRY, ModalitySpec
+from torch_brain.registry import MODALITIY_REGISTRY
 from torch_brain.models.poyo import POYO
 from torch_brain.utils import callbacks as tbrain_callbacks
 from torch_brain.utils import seed_everything
